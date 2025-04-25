eb0507afda2bf41dd5e63f5ebb743a16ec94a141
ZS912719 <shengxun.zhao@gmail.com>
2025-04-25 15:01:43 +0200

    Initial calls for GraphMap in main

--- src/main/java/org/ravioles/Main.java
+++ src/main/java/org/ravioles/Main.java
@@ -1,7 +1,24 @@
 package org.ravioles;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.ui.spriteManager.SpriteManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("org.graphstream.ui", "swing");
+        Timer timer = new Timer();
+        GraphMap graphMap = new GraphMap(timer);
+        Graph graph = graphMap.getGraph();
+        graph.display();
+
+        SpriteManager sman = new SpriteManager(graph);
     }
 }
