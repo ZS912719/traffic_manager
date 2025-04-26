e0a4df5d1d151218c364f6051373770ab8141941
ZS912719 <shengxun.zhao@gmail.com>
2025-04-26 17:20:03 +0200

    create traffic light for intersections

---
+++ src/main/java/org/ravioles/agents/TrafficLight.java
@@ -0,0 +1,84 @@
+package org.ravioles.agents;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TrafficLight {
+    private final Map<String, Map<String, List<Transition>>> transitions;
+    Node tl1;
+    Node tl2;
+    Node tl3;
+    Node tl4;
+    Graph graph;
+    List<String> actions = List.of("a_1100", "a_0100", "a_1000", "a_0011", "a_0010", "a_0001");
+    // Explanation by example: a_0100 means road 1,3,4 are not passable while road 2 is passable.
+    // The option should be seen as action_(1)0(2)1(3)0(4)0 where 0 means not passable and 1 means passable.
+    String action;
+    List<Edge> intersections;
+    ArrayList<String> states = new ArrayList<String>();
+    String state;
+    int timer = 0; // universal ticking timer , updated by the class Timer of this project
+
+    public TrafficLight(Graph graph, Node tl1, Node tl2, Node tl3, Node tl4) {
+        this.transitions = new HashMap<>();
+
+        this.graph = graph;
+
+        this.tl1 = tl1;
+        this.tl2 = tl2;
+        this.tl3 = tl3;
+        this.tl4 = tl4;
+
+        this.intersections = getIntersections();
+
+    }
+
+    public Node getTL1() {
+        return this.tl1;
+    }
+    public Node getTL2() {
+        return this.tl2;
+    }
+    public Node getTL3() {
+        return this.tl3;
+    }
+    public Node getTL4() {
+        return this.tl4;
+    }
+
+    public List<Edge> getIntersections() {
+        return List.of(
+                tl1.getEdgeBetween(tl2.getId()),
+                tl2.getEdgeBetween(tl3.getId()),
+                tl3.getEdgeBetween(tl4.getId()),
+                tl4.getEdgeBetween(tl1.getId())
+        );
+    }
+
+    public void takeAction() {
+
+    }
+
+    public double calculateReward() {
+        // let a vehicle pass is 10pts
+        // keep a vehicle waiting is -1pts/s then exponential decay
+
+        return 0;
+    }
+
+    public void updateTimer(int tic) {
+        this.timer += tic;
+    }
+
+    public double checkTraffic() {
+        return 0;
+    }
+
+
+}
