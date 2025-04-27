8a465155f249e7dc319b87d9c19ea998b1befdd0
ZS912719 <shengxun.zhao@gmail.com>
2025-04-27 12:15:04 +0200

    update main

--- src/main/java/org/ravioles/Main.java
+++ src/main/java/org/ravioles/Main.java
@@ -1,24 +1,34 @@
 package org.ravioles;
 
-import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
-import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.ui.spriteManager.SpriteManager;
+import org.ravioles.agents.Vehicle;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main{
+    public static volatile int tic = 0;
 
     public static void main(String[] args) {
+        int delay = 100;
+        Timer timer = new Timer(delay, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tic++;
+            }
+        });
+        timer.setInitialDelay(0);
+        timer.start();
+
         System.setProperty("org.graphstream.ui", "swing");
-        Timer timer = new Timer();
-        GraphMap graphMap = new GraphMap(timer);
+        GraphMap graphMap = new GraphMap();
         Graph graph = graphMap.getGraph();
+        SpriteManager sman = graphMap.getSpriteManager();
+
+        Vehicle car1 = new Vehicle(graphMap, sman, "Demo", graph.getNode("b1"), graph.getNode("d4"), 0.2, timer);
         graph.display();
 
-        SpriteManager sman = new SpriteManager(graph);
     }
 }
