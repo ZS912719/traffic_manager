1c0187c6d12af2d3b3d0b83d975a8b7ac69daecc
ZS912719 <shengxun.zhao@gmail.com>
2025-04-27 18:44:33 +0200

    simulate 3 moving vehicles and toggle lights

--- src/main/java/org/ravioles/Main.java
+++ src/main/java/org/ravioles/Main.java
@@ -2,16 +2,21 @@ package org.ravioles;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.ui.spriteManager.SpriteManager;
+import org.ravioles.agents.Intersection;
 import org.ravioles.agents.Vehicle;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class Main{
     public static volatile int tic = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int delay = 100;
         Timer timer = new Timer(delay, new ActionListener() {
             @Override
@@ -26,9 +31,35 @@ public class Main{
         GraphMap graphMap = new GraphMap();
         Graph graph = graphMap.getGraph();
         SpriteManager sman = graphMap.getSpriteManager();
-
-        Vehicle car1 = new Vehicle(graphMap, sman, "Demo", graph.getNode("b1"), graph.getNode("d4"), 0.2, timer);
         graph.display();
 
+        Intersection i1 = new Intersection
+                (
+                        graphMap,
+                        graph.getNode("c2"),
+                        graph.getNode("b2"),
+                        graph.getNode("b3"),
+                        graph.getNode("c3")
+                );
+
+        graphMap.addTrafficLight(i1);
+        i1.toggleLight("s_10?");
+
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        sleep(3000);
+        Vehicle car1 = new Vehicle(graphMap, sman, "Demo1", graph.getNode("b1"), graph.getNode("d4"), 0.2, timer);
+        vehicles.add(car1);
+
+        sleep(3000);
+        Vehicle car2 = new Vehicle(graphMap, sman, "Demo2", graph.getNode("d4"), graph.getNode("b1"), 0.2, timer);
+        vehicles.add(car2);
+
+        sleep(3000);
+        Vehicle car3 = new Vehicle(graphMap, sman, "Demo3", graph.getNode("c1"), graph.getNode("d4"), 0.2, timer);
+        vehicles.add(car3);
+
+        sleep(3000);
+        i1.toggleLight("s_01");
     }
 }
