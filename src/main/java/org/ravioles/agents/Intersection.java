8edf06d3c2b384ca6ae3545587d9f9709f1f426d
ZS912719 <shengxun.zhao@gmail.com>
2025-04-27 18:41:28 +0200

    Enable the traffic lights to toggle

--- src/main/java/org/ravioles/agents/Intersection.java
+++ src/main/java/org/ravioles/agents/Intersection.java
@@ -3,15 +3,18 @@ package org.ravioles.agents;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.ravioles.GraphMap;
+import org.ravioles.Main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Intersection {
+public class Intersection implements ActionListener {
     private final Map<String, Map<String, List<Transition>>> transitions;
-    Node tl1;
+    Node tl1; // traffic light
     Node tl2;
     Node tl3;
     Node tl4;
@@ -21,9 +24,10 @@ public class Intersection {
     // Action "a_01?" means road 1,3 are passable while road 2,4 are not but they are switching (orange light).
     String action;
     List<Edge> intersections;
-    ArrayList<String> states = new ArrayList<String>();
+    List<String> states = List.of("s_10","s_10?","s_01","s_01?");
     String state;
-    int timer = 0; // universal ticking timer , updated by the class Timer of this project
+    int timer = 0; // timer for switching light
+    int currentTick;
 
     public Intersection(GraphMap graph, Node tl1, Node tl2, Node tl3, Node tl4) {
         this.transitions = new HashMap<>();
@@ -64,8 +68,64 @@ public class Intersection {
         );
     }
 
-    public void takeAction() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int oldTick = this.currentTick;
+        this.currentTick = Main.tic;
+        int deltaTicks = this.currentTick - oldTick;
 
+        act();
+    }
+
+    public void act() {
+
+    }
+
+    public void toggleLight(String newState){
+        this.state = newState;
+        switch (this.state) {
+            case "s_10":
+                this.tl1.setAttribute("ui.style", "fill-color: rgb(0,255,0);"); //green
+                this.tl1.setAttribute("color", "green");
+                this.tl3.setAttribute("ui.style", "fill-color: rgb(0,255,0);");
+                this.tl3.setAttribute("color", "green");
+                this.tl2.setAttribute("ui.style", "fill-color: rgb(255,0,0);"); // red
+                this.tl2.setAttribute("color", "red");
+                this.tl4.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl4.setAttribute("color", "red");
+                break;
+            case "s_10?":
+                this.tl1.setAttribute("ui.style","fill-color: rgb(255,128,0);"); // orange
+                this.tl1.setAttribute("color", "orange");
+                this.tl3.setAttribute("ui.style","fill-color: rgb(255,128,0);");
+                this.tl3.setAttribute("color", "orange");
+                this.tl2.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl2.setAttribute("color", "red");
+                this.tl4.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl4.setAttribute("color", "red");
+                break;
+            case "s_01":
+                this.tl1.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl1.setAttribute("color", "red");
+                this.tl3.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl3.setAttribute("color", "red");
+                this.tl2.setAttribute("ui.style", "fill-color: rgb(0,255,0);");
+                this.tl2.setAttribute("color", "green");
+                this.tl4.setAttribute("ui.style", "fill-color: rgb(0,255,0);");
+                this.tl4.setAttribute("color", "green");
+                break;
+            case "s_01?":
+                this.tl1.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl1.setAttribute("color", "red");
+                this.tl3.setAttribute("ui.style", "fill-color: rgb(255,0,0);");
+                this.tl3.setAttribute("color", "red");
+                this.tl2.setAttribute("ui.style", "fill-color: rgb(255,128,0);");
+                this.tl2.setAttribute("color", "orange");
+                this.tl4.setAttribute("ui.style", "fill-color: rgb(255,128,0);");
+                this.tl4.setAttribute("color", "orange");
+                break;
+        }
+        this.timer = 0;
     }
 
     public double calculateReward() {
@@ -81,6 +141,10 @@ public class Intersection {
 
     public double checkTraffic() {
         return 0;
+    }
+
+    public List<Node> getTrafficLights() {
+        return List.of(tl1, tl2, tl3, tl4);
     }
 
 
