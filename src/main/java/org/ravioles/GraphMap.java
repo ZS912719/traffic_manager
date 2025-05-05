14e28a9647f9a55a1d11cbc8bf6199867bb416d5
ZS912719 <shengxun.zhao@gmail.com>
2025-05-05 15:58:32 +0200

    update GraphMap

--- src/main/java/org/ravioles/GraphMap.java
+++ src/main/java/org/ravioles/GraphMap.java
@@ -7,26 +7,34 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.ui.spriteManager.Sprite;
 import org.graphstream.ui.spriteManager.SpriteManager;
-import org.ravioles.agents.Intersection;
+import org.ravioles.agents.Vehicle;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
-public class GraphMap {
+public class GraphMap implements ActionListener {
+    private final Timer timer;
     Graph graph;
     SpriteManager sman;
     List<Intersection> intersections = new ArrayList<>();
-    HashMap<Edge, Integer> roadPassiblities = new HashMap<>();
+    List<Vehicle> vehicles = new ArrayList<>();
 
-    public GraphMap() {
+    int currentTick;
+
+    public GraphMap(Timer timer) {
+        this.timer = timer;
+        timer.addActionListener(this);
+
         this.graph = new MultiGraph("Traffic Management System");
         this.sman = new SpriteManager(graph);
 
-//        List<String> cols = List.of("a", "b", "c", "d", "e", "f", "g", "h");
-        List<String> cols = List.of("a", "b", "c", "d");
-        int rowCount = 4;
+        List<String> cols = List.of("a", "b", "c", "d", "e", "f", "g", "h");
+//        List<String> cols = List.of("a", "b", "c", "d");
+        int rowCount = 8;
 
         // the pointes are noted like a1, b2
         for (int ci = 0; ci < cols.size(); ci++) {
@@ -51,7 +59,7 @@ public class GraphMap {
                 }
                 Edge e = graph.addEdge(src + dst, src, dst, true);
                 e.setAttribute("distance", 10.0); // each edge has a distance which by default is 10
-                e.setAttribute("congestion", 0.0); // this shall be the index of certain number
+                e.setAttribute("congestion", 0); // this shall be the index of certain number
             }
         }
 
@@ -66,10 +74,9 @@ public class GraphMap {
                 String dst = order.get(j + 1) + r;
                 Edge e = graph.addEdge(src + dst, src, dst, true);
                 e.setAttribute("distance", 10.0);
-                e.setAttribute("congestion", 0.0);
+                e.setAttribute("congestion", 0);
             }
         }
-
     }
 
     public void addTrafficLight(Intersection intersection) {
@@ -96,7 +103,7 @@ public class GraphMap {
 
         for (Edge e : intersections) {
             e.setAttribute("distance", 1.0);
-            e.setAttribute("congestion", 0.0);
+            e.setAttribute("congestion", 0);
         }
     }
 
@@ -108,7 +115,16 @@ public class GraphMap {
         return this.graph;
     }
 
-    public void updateTimer() {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.currentTick = Main.tic;
+
+        act();
+    }
+
+    public void act(){
+        updateCongestion();
     }
 
     /**
@@ -131,13 +147,13 @@ public class GraphMap {
                 Edge e = (Edge) attach;
                 if (!intersections.contains(e)) { // the congestion level of an intersection shall always be 0
                     Integer cnt = e.getAttribute("congestion");
-                    e.setAttribute("congestion", (cnt == null ? 0.0 : cnt) + 1.0);
+                    e.setAttribute("congestion", (cnt == null ? 0 : cnt) + 1);
                 }
             } else if (attach instanceof Node n) {
                 for (Edge e : n.getEachLeavingEdge()) {
                     if (!intersections.contains(e)) {
                         Integer cnt = e.getAttribute("congestion");
-                        e.setAttribute("congestion", (cnt == null ? 0.0 : cnt) + 1.0);
+                        e.setAttribute("congestion", (cnt == null ? 0 : cnt) + 1);
                     }
                 }
             }
@@ -148,19 +164,18 @@ public class GraphMap {
      * the passibility of a road is calculated by multiplying its distance by 1.5 raised to the power of the congestion value
      */
     public double calculatePassibility(Edge e) {
-        return ((double) e.getAttribute("distance")) * Math.pow(1.5, e.getAttribute("congestion"));
-    }
-
-    public List<Sprite> getSprites(){
-        List<Sprite> sprites = new ArrayList<>();
-        for (Sprite s : sman.sprites()) {
-            if (!s.attached()) continue;
-            sprites.add(s);
-        }
-        return sprites;
+        return ((double) e.getAttribute("distance")) * Math.pow(1.5, (int)e.getAttribute("congestion"));
     }
 
     public List<Intersection> getIntersections() {
         return this.intersections;
+    }
+
+    public void addToVehicles(Vehicle v){
+        this.vehicles.add(v);
+    }
+
+    public List<Vehicle> getVehicles(){
+        return this.vehicles;
     }
 }
